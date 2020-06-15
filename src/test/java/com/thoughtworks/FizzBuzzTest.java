bc532eadad7b5ef4e6f75bf06d1429f404832181
huyuanixn325 <huyuanxin0325@163.com>
2020-06-15 14:51:13 +0800

    finish should_return_Fizz_when_input_3

--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -8,11 +8,21 @@ public class FizzBuzzTest {
     public void should_return_1_when_input_1(){
         //given
         FizzBuzz fizzBuzz = new FizzBuzz();
+        int number = 1;
         //when
-        String result = fizzBuzz.say(1);
+        String result = fizzBuzz.say(number);
         //then
         Assert.assertEquals("1",result);
     }
 
-
+    @Test
+    public void should_return_Fizz_when_input_3(){
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int number = 3;
+        //when
+        String result = fizzBuzz.say(number);
+        //then
+        Assert.assertEquals("Fizz",result);
+    }
 }
